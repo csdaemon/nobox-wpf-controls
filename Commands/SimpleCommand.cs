23ae3c865669ea5f27d37c0dcce331235b3da82d
csdaemon <86532783+csdaemon@users.noreply.github.com>
2023-12-19 17:09:02 -0500

    Cleanup
    Cleanup

--- Commands/SimpleCommand.cs
+++ Commands/SimpleCommand.cs
@@ -22,6 +22,10 @@ namespace Nobox.WPF.Commands
                 Executed(this, parameter);
         }
 
-        public event EventHandler CanExecuteChanged;
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
     }
 }
